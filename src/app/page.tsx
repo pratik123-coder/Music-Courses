c06b66ae4cd09c263e8f80385b44df395d93e387
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-03-29 22:15:43 +0530

    Hero Section Done || Tailwind Config Bug Fixed || Aceternity UI Bug Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white[0.02]">
-        <h1 className="text-2xl text-center">Hi</h1>
         <Hero />
     </main>
 
